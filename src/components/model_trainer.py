c92dbcfa16a6f70aaee9283ec34e3dda196e8bab
Monish Nallagondalla <nsmonish@gmail.com>
2024-12-29 22:41:35 +0530

    model trainer commit

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -13,3 +13,26 @@ from src.exception import CustomException
 from src.logger import logging
 from src.utils import evaluate_models , load_object , save_object , upload_file 
 
+@dataclass
+
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+
+class CustomModel:
+    def __init__(self,X):
+        transformed_feature = self.preprocessing_object.transform(X)
+
+        return self.trained_model_object.predict(transformed_feature)
+    
+    def __repr__(self):
+        return f"{type(self.trained_model_object).__name__}()"
+    
+    def __str__(self):
+        return f"{type(self.trained_model_object).__name__}()"
+    
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    
