9930daffebb3f3c2b2ece15dd61e7d6f1f6cdc80
Monish Nallagondalla <nsmonish@gmail.com>
2025-01-04 23:33:06 +0530

    update models

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,7 +2,9 @@ import os
 import sys
 from dataclasses import dataclass
 
-from sklearn.ensemble import RandomForestClassifier,GradientboostingClassifier,Adaboostclassifier 
+from sklearn.ensemble import (AdaBoostClassifier,
+    GradientBoostingClassifier,
+    RandomForestClassifier,)
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -36,4 +38,23 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    
+    def initiate_model_trainer(self,train_array,test_array,preprocessor_path):
+        try:
+            logging.info(f"Splitting training and testing input and target feature")
+            x_train, y_train,x_test,y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+
+            models= {
+                "Random Forest": RandomForestClassifier(),
+                "Decision Tree": DecisionTreeClassifier(),
+                "Gradient Boosting": GradientBoostingClassifier(),
+                "K-Neighbors Classifier": KNeighborsClassifier(),
+                "XGBClassifier": XGBClassifier(),
+                "AdaBoost Classifier": AdaBoostClassifier(),
+            }
+
+            logging.info(f"Extracting model config file path")
